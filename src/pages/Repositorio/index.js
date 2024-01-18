ba5c81461ea38476ec2c3f71846e5e23299b0533
ArthurSalesFM <sales.arthur.fm@gmail.com>
2024-01-18 18:51:57 -0300

    enviando arquivos de visualização das issues.

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Owner, Loading, BackButton } from "./styles";
+import { Container, Owner, Loading, BackButton, IssuesList } from "./styles";
 import api from '../../services/api';
 import { FaArrowLeft } from 'react-icons/fa';
 
@@ -50,14 +50,36 @@ export default function Repositorio({match}){
         <Container >
 
             <BackButton to='/'>
+
                 <FaArrowLeft color='red' size={30}/>
+
             </BackButton>
             
             <Owner>
+
                 <img src={repositorio.owner.avatar_url} alt={repositorio.owner.login}/>
                 <h1>{repositorio.name}</h1>
                 <p>{repositorio.description}</p>
+
             </Owner>
+
+            <IssuesList>
+                {issues.map( issue => (                    
+                    <li key={ String ( issue.id ) }>                        
+                        <img src={issue.user.avatar_url} alt={issue.user.login}></img>
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>{issue.title}</a>
+                                {issue.labels.map( label => (
+                                    <span key={String(label.id)}>{label.name}</span>
+                                ))}
+                            </strong>
+                            <p>{issue.user.login}</p>
+                        </div>                    
+                    </li>
+                ))}
+
+            </IssuesList>
                 
         </Container>
         
